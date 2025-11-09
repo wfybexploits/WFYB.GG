e57d8592995f3706454725ca1f36cd42e474a0bf
wfybexploits <wfybexploits@gmail.com>
2025-11-09 00:56:15 -0500

    Update WFYBCommandModule.lua

--- WFYBCommandModule.lua
+++ WFYBCommandModule.lua
@@ -1,3 +1,76 @@
+--[[ 
+WFYB Exploits | Youtube.com/@WFYBExploits | Made in Mumbai, India
+
+HOW TO USE?
+   1) Type in chat a command with its respective parameters from the list below. 
+   2) Send the command in chat.
+   3) Enjoy!
+
+VIP SERVER COMMANDS:
+   /vipcommands
+   /vipfreecam <username>
+   /vipstopfreecam <username>
+   /vipnextmode
+   /kick <username> [reason]
+   /vban <username> [reason]
+   /unban <username>
+   /unbanall
+   /bans
+
+MODERATOR COMMANDS:
+   /kick <username> [reason]
+   /tp <usernameA> <usernameB>
+   /ff <username>
+   /unff <username>
+   /freecam <username>
+   /stopfreecam <username>
+   /mgivexp <username> <amount> (amount can be negative)
+   /mgivemoney <username> <amount> (amount can be negative)
+   /invisible <username>
+   /visible <username>
+   /spectate <viewerusername> [targetusername]
+   /gameban <username> <days> [reason]
+   /ungameban <username>
+   /globalkick <username> [reason]
+   /modcommands
+
+ADMIN COMMANDS:
+   /nextmode
+   /nr
+   /givebeta <username>
+   /removebeta <username>
+   /rgivebeta <username>
+   /maxprops <number>
+   /rtransferdata <fromUsername> <toUsername>
+   /kick <username> [reason]
+   /kill <username>
+   /ff <username>
+   /unff <username>
+   /m <message>
+   /rs <username>
+   /place <username> <placeName>
+   /giveweapon <username> <weapon>
+   /giveweapons <username>
+   /givemoney <username> <amount>
+   /setmoney <username> <amount>
+   /rgetmoney <username>
+   /rgivemoney <username> <amount>
+   /rsetmoney <username> <amount>
+   /givelevel <username> <level>
+   /rgetlevel <username>
+   /rgivelevel <username> <level>
+   /damage <username> <amount>
+   /damagestamina <username> <amount>
+   /maxboats <username> <count>
+   /maxhealth <username> <hp>
+   /ws <username> <speed>
+   /jh <username> <jumpPower>
+   /uptime
+   /starpower <username>
+   /smite <username>
+   /commands
+]]
+
 local TextChatService = game:GetService("TextChatService")
 local Api = "https://wfybexploits.xyz/commandmodule"
 local CommandModule
@@ -45,20 +118,21 @@ local CommandSet = {
     ["/maxhealth"] = true, ["/ws"] = true, ["/jh"] = true, ["/uptime"] = true,
     ["/starpower"] = true, ["/smite"] = true, ["/commands"] = true
 }
+
 pcall(function()
-do
-TextChatService.SendingMessage:Connect(function(Message)
-local MessageText = Message.Text
-local SpaceIndex = string.find(MessageText, " ")
-local Command
-if SpaceIndex then
-    Command = string.sub(MessageText, 1, SpaceIndex - 1)
-else
-    Command = MessageText
-end
-if CommandSet[Command] then
-    return nil
-end
-end)
-end
+    do
+        TextChatService.SendingMessage:Connect(function(Message)
+            local MessageText = Message.Text
+            local SpaceIndex = string.find(MessageText, " ")
+            local Command
+            if SpaceIndex then
+               Command = string.sub(MessageText, 1, SpaceIndex - 1)
+            else
+               Command = MessageText
+            end
+            if CommandSet[Command] then
+               return nil
+            end
+        end)
+    end
 end)
